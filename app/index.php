74c31423c6fcee7a9eb91db845c3e312a388debc
Leonardo Sampaio <leonardors@gmail.com>
2021-12-08 11:53:53 -0300

    maldito docker

--- app/index.php
+++ app/index.php
@@ -2,11 +2,12 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$client = new MongoDB\Client(
-
-    'mongodb://user:password@localhost:27017'
-
-);
-$collection = $client->test->default;
-$collection->insertOne(array('nome' => 'sdsd'));
-var_dump($collection);
+$client = new MongoDB\Client('mongodb://user:password@db:27017');
+$db = $client->default;
+$collection = $db->messages;
+$collection->insertOne(array('name' => 'test'));
+$records = $collection->find( [ 'name' => 'test'] );  
+foreach ($records as $record)
+{  
+    var_dump($record);
+}
